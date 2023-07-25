e6ebe930b190aac91bd3fde49cb021ea3407abdc
sravaniravipati18 <139853758+sravaniravipati18@users.noreply.github.com>
2023-07-25 18:06:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ except URLError as e:
   streamlit.error()
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
 
- 
+ # write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
  
