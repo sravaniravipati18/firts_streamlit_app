d1e8aa63a6393ed8336e6442aeaff157839e22f1
sravaniravipati18 <139853758+sravaniravipati18@users.noreply.github.com>
2023-08-13 02:02:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,11 +65,14 @@ import snowflake.connector
 # streamlit.header("The fruit load list contains:")
 # streamlit.dataframe(my_data_rows)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-add_my_fruit = streamlit.text_input('What fruit would you like add?')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_cur.execute("insert into fruit_load_list values ('"+add_my_fruit+ "')")
-streamlit.write('Tankyou for adding', add_my_fruit) 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
+add_my_fruit = streamlit.text_input('what fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+#streamlit.write('Thanks for adding jackfruit')
